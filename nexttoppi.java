422f91baac535c10150fe9f1ce38d5c331a5612b
Pepeu <75397460+PedroPepeu@users.noreply.github.com>
2022-05-01 21:59:18 -0300

    now it's work
    I still have a problem with high value numbers, but it's whorking, I thing it's my computer.
    I've changed the format int to double, now everything is double, and add and interation + 1 inside the true part in "isInsideCircle".
    I also changed the println to prinf using numbers after the comma as far as I could.

--- nexttoppi.java
+++ nexttoppi.java
@@ -7,7 +7,7 @@ public class nexttopi {
 
         Scanner sc = new Scanner (System.in);
 
-        int n, interation =0, nPointsInCircle=0;
+        double n, interation =0, nPointsInCircle=0;
         double x=0, y=0, PI=0;
 
         n = sc.nextInt();
@@ -16,10 +16,11 @@ public class nexttopi {
             x = (Math.random() * 2) - 1;
             y = (Math.random() * 2) - 1;
 
-            boolean isInsideCircle = Math.pow(x, 2) + Math.pow(y, 2) <=1;
+            boolean isInsideCircle = Math.pow(x, 2) + Math.pow(y, 2) <= 1;
 
             if(isInsideCircle) {
                 nPointsInCircle++;
+                interation++;
             } else {
                 interation++;
             }
@@ -27,7 +28,7 @@ public class nexttopi {
         
         PI = (4* nPointsInCircle) / n;
 
-        System.out.println(PI);
+        System.out.printf("%f\n", PI);
 
         
 
