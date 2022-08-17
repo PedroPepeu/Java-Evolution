e3bdae6c74ae84d623e9156e13fcec992c50c3cb
Freitas <pedrosteamgtav@gmail.com>
2022-08-17 17:28:39 -0300

    first recursion lesson, infinit recursion

---
+++ src/recurs1.java
@@ -0,0 +1,12 @@
+public class recurs1 {
+    public static void main(String[] args) {
+
+        p();
+
+    }
+
+    static void p() {
+        System.out.println("recursion");
+        p();
+    }
+}
